0e972964a40338a4a9f74120f16fb589bb21483b
Bushra Taibani <bushrataibani1@gmail.com>
2023-08-31 18:58:44 +0530

    init next & navbar, hero, footer section

---
+++ types/index.ts
@@ -0,0 +1,8 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+  title: string;
+  btnType?: "button" | "submit";
+  containerStyle?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
